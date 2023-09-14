ad962e3a57a0eaaa5df3deaebc7d5d4156533145
unknown <tmarinkie12@gmail.com>
2023-09-14 21:06:21 +0200

    Added navigation demo page details

--- screens/DemoScreen.tsx
+++ screens/DemoScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text,SafeAreaView ,TouchableOpacity} from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
 import React from 'react'
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { RootStackParamList } from '../App'
@@ -6,20 +6,31 @@ import { useNavigation } from '@react-navigation/native';
 import { Ionicons } from '@expo/vector-icons';
 
 export type NavigationProp = NativeStackNavigationProp<
-RootStackParamList,"Demo">;
+    RootStackParamList, "Demo">;
 
 //call in hook
-const DemoScreen = ()=>{
+const DemoScreen = () => {
     const navigation = useNavigation<NavigationProp>();
-  return (
-    <SafeAreaView className='bg-[#E59620] flex-1'>
-        <TouchableOpacity onPress={navigation.goBack} className='flex-row items-start p-5'>
-            <Ionicons name="arrow-back" size={50} color="white"></Ionicons>
-            <Text className='text-white'>Go Back</Text>
-        </TouchableOpacity>
-      <Text>DemoScreen</Text>
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView className='bg-[#E59620] flex-1'>
+            <TouchableOpacity onPress={navigation.goBack} className='flex-row items-start p-5'>
+                <Ionicons name="arrow-back" size={50} color="white"></Ionicons>
+                <Text className='text-white'>Go Back</Text>
+            </TouchableOpacity>
+
+            <View className='flex-1 items-center justify-center px-10'>
+                <Text className='text-white text-2xl font-extrabold'>Hooray!</Text>
+                <Text className='text-white text-2xl font-extrabold'>You have access to this feature!</Text>
+                <Ionicons name='build-outline' size={200} color="white"></Ionicons>
+                <View>
+                    <Ionicons name='checkmark-circle-sharp' size={60} color="#96f550"></Ionicons>
+                </View>
+                <Text className='text-white mt-10 flex-1 font-bold text-center'> This is where the feature page and its details go.
+                    navigation.navigate('NextSpecifiedPage')</Text>
+            </View>
+
+        </SafeAreaView>
+    );
 };
 
 export default DemoScreen;
