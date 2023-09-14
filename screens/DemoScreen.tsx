f5462e37a560153b9334af5f6e9ac5b4cee06fdf
unknown <tmarinkie12@gmail.com>
2023-09-14 20:39:59 +0200

    Demo screen navigate back functionality

--- screens/DemoScreen.tsx
+++ screens/DemoScreen.tsx
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
+import { View, Text,SafeAreaView ,TouchableOpacity} from 'react-native'
 import React from 'react'
+import { NativeStackNavigationProp } from '@react-navigation/native-stack'
+import { RootStackParamList } from '../App'
+import { useNavigation } from '@react-navigation/native';
+import { Ionicons } from '@expo/vector-icons';
 
-const DemoScreen = () => {
+export type NavigationProp = NativeStackNavigationProp<
+RootStackParamList,"Demo">;
+
+//call in hook
+const DemoScreen = ()=>{
+    const navigation = useNavigation<NavigationProp>();
   return (
-    <View>
+    <SafeAreaView className='bg-[#E59620] flex-1'>
+        <TouchableOpacity onPress={navigation.goBack} className='flex-row items-start p-5'>
+            <Ionicons name="arrow-back" size={50} color="white"></Ionicons>
+            <Text className='text-white'>Go Back</Text>
+        </TouchableOpacity>
       <Text>DemoScreen</Text>
-    </View>
-  )
-}
+    </SafeAreaView>
+  );
+};
 
-export default DemoScreen
+export default DemoScreen;
