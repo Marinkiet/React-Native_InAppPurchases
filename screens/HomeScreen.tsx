95a9e450abd47abfbc67f7c6fa928d253b210d8e
unknown <tmarinkie12@gmail.com>
2023-09-14 13:35:16 +0200

    set up with homescreen nav

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,25 @@
+import { View, Text,Image } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native'
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaView >
+      <Text className="">Home Screen Created</Text>
+      {/* Icon Pro/Upgrade to Pro */}
+      {/* Image */}
+      <Image
+      source={{uri:"https://i.imgur.com/e14NE49.png"}}
+      className="w-full h-64"/>
+      {/* View flexed into two Action Row */}
+      {/* ActionRow */}
+      {/* ActionRow */}
+      {/* ActionRow */}
+      {/* ActionRow */}
+      {/* ActionRow */}
+      {/* ActionRow */}
+      {/* ActionRow */}
+
+    </SafeAreaView>
+  )
+}
